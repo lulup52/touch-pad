4c94d134444d19ab4d8e0a9f3482ee29d60bb37e
lulup52 <lulu-p52@hotmail.fr>
2021-08-12 21:11:50 +0200

    test d'affichage gh pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import JeuDechec from './components/newFunctionality/JeuDechec'
 function App() {
   return (
     <div >
+      hello
       <Router>
         <Switch>
           <Route path ="/menu"><Menu /></Route>
