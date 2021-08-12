90fdb7c5baedd00d1da684edd43886f6a02ae3ca
lulup52 <lulu-p52@hotmail.fr>
2021-08-12 21:15:16 +0200

    Updates

---
+++ precache-manifest.fac66298107a0ac5a03d817010a5e5a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2631db1151e56c789710fab0885c5f3",
+    "url": "/touch-pad/menu/index.html"
+  },
+  {
+    "revision": "994f3133f30d547e8ae3",
+    "url": "/touch-pad/menu/static/css/2.e3f36e05.chunk.css"
+  },
+  {
+    "revision": "412694cdea48a33bef1a",
+    "url": "/touch-pad/menu/static/css/main.66d5ab0c.chunk.css"
+  },
+  {
+    "revision": "994f3133f30d547e8ae3",
+    "url": "/touch-pad/menu/static/js/2.03987393.chunk.js"
+  },
+  {
+    "revision": "ca96dfca6b4a2ebbcbc55ce92a445069",
+    "url": "/touch-pad/menu/static/js/2.03987393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412694cdea48a33bef1a",
+    "url": "/touch-pad/menu/static/js/main.24b5ff89.chunk.js"
+  },
+  {
+    "revision": "2654c8794ff60b84b99a",
+    "url": "/touch-pad/menu/static/js/runtime-main.28e1e035.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/touch-pad/menu/static/media/logo.ee7cd8ed.svg"
+  }
+]);
